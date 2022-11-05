f4bfedf2c75e8253e83a983cf82b68b5585d4c87
Rafael Alberto Larios <albertolarios@me.com>
2022-11-04 20:20:57 -0600

    Add "addBookToLibrary" function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello, world!")
+let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -6,12 +6,28 @@ function Book(title, author, pages, isRead) {
     this.pages = pages;
     this.isRead = isRead;
     this.info = () => {
-        if (this.isRead) { read = "read" } else { read = "not read yet" }
+        if ( this.isRead ) { read = "read" } else { read = "not read yet" }
         return `The ${this.title} by ${this.author}, ${this.pages} pages, ${read}.`;
     };
 }
 
-const republic = new Book("Republic", "Plato", 1081, true);
-const harryPotter = new Book("Harry Potter", "J. K. Rowling", 525, false);
-console.log(republic.info())
-console.log(harryPotter.info())
+function addBookToLibrary () {
+    let title = prompt("Enter book's title:");
+    let author = prompt("Enter book's author:")
+    let pages = prompt("Enter number of pages:")
+    let read = "";
+    let isRead;
+    while ( read.length !== 1 ) {
+        read = prompt("Have you read it? (y/n)");
+    }
+    read = read.toLowerCase();
+    if ( read === "y" ) {
+        isRead = true;
+    }
+    else if ( read === "n" ) {
+       isRead = false;
+    }
+    newBook = new Book(title, author, pages, isRead);
+
+    myLibrary.push(newBook)
+}
